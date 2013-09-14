68cb5dc78295e2cc4b110173d1865946dfdf6898
Mehdi Khalili <me@mehdi-khalili.com>
2013-09-14 15:17:41 +0430

    added error handling WIP

--- web.js
+++ web.js
@@ -2,29 +2,25 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+// catch all
+app.use(express.static(__dirname + '/out'));
+
+// Html files to urls
 app.get('/routes.json', function (req, res) {
     res.status(403).send('403 Forbidden');
 })
 
-app.use(express.static(__dirname + '/out'));
-
 var routes = require('./out/routes.json').routes;
-
-var redirector = function (dest) {
-    return function (req, res) {
-        res.redirect(301, dest);
-    };
-};
-
 routes.map(function (route) { 
 	app.get(route.url, function(req, res) { 
 		res.sendfile(__dirname + '/out' + route.file); }); 
 });
 
 app.get(/^\/tagged\/(\w+)$/, function (req, res) {
-    res.redirect(301, req.path + '.html');
+    res.sendfile(__dirname + '/out' + req.path + '.html');
 });
 
+// Feed
 var feed = function(req, res) {
   fs.readFile(__dirname + '/out/feed.xml', 'utf8', function (err, data) {
       res.set('Content-Type', 'application/xml');
@@ -34,5 +30,11 @@ var feed = function(req, res) {
 
 app.get('/feed', feed);
 app.get('/feeds/rss', feed);
+
+// Error Handling
+app.use(function (err, req, res, next) {
+  console.log('errored');
+  res.status(404).sendfile('/out/404.html');
+});
 
 app.listen(process.env.PORT || 3000);
